f91b52a3a2b89aa89036525c5e64fb440334fed1
Michael van Tricht <metricht@gmail.com>
2022-10-30 16:00:11 +0100

    Update back.js

--- back.js
+++ back.js
@@ -3,20 +3,20 @@ const fs = require("fs");
 const { app } = require("electron");
 
 const writeToFile = (content) => {
-	fs.writeFile(process.env.HOMEDRIVE + process.env.HOMEPATH + "\\foobar_np.txt", content, () => {});
+fs.writeFile(process.env.HOMEDRIVE + process.env.HOMEPATH + "\\foobar_np.txt", content, () => {});
 };
 
 module.exports = () => {
-	registerCallback((songInfo) => {
-    let output = "stopped";
-		if (songInfo.title && songInfo.artist) {
-      output = songInfo.isPaused ? 'stopped' : (`playing: ${songInfo.artist} - ${songInfo.title}`);
-		}
-		writeToFile(output);
-	});
-	app.on('window-all-closed', module.exports.clear)
+    registerCallback((songInfo) => {
+        let output = "stopped";
+        if (songInfo.title && songInfo.artist) {
+            output = songInfo.isPaused ? 'stopped' : (`playing: ${songInfo.artist} - ${songInfo.title}`);
+        }
+        writeToFile(output);
+    });
+    app.on('window-all-closed', module.exports.clear)
 };
 
 module.exports.clear = () => {
-	writeToFile("stopped"); 
+    writeToFile("stopped"); 
 };
