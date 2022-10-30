97d34a7b5bb7adeef627475b02be5be07cd718c0
Michael van Tricht <metricht@gmail.com>
2022-10-30 15:43:36 +0100

    Create back.js

---
+++ back.js
@@ -0,0 +1,22 @@
+const registerCallback = require("../../providers/song-info");
+const fs = require("fs");
+const { app } = require("electron");
+
+const writeToFile = (content) => {
+	fs.writeFile(process.env.HOMEDRIVE + process.env.HOMEPATH + "\\foobar_np.txt", content, () => {});
+};
+
+module.exports = () => {
+	registerCallback((songInfo) => {
+    let output = "stopped";
+		if (songInfo.title && songInfo.artist) {
+      output = songInfo.isPaused ? 'stopped' : (`playing: ${songInfo.artist} - ${songInfo.title}`);
+		}
+		writeToFile(output);
+	});
+	app.on('window-all-closed', module.exports.clear)
+};
+
+module.exports.clear = () => {
+	writeToFile("stopped"); 
+};
